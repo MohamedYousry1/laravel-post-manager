51fb32dfe6b6c57f0fe690532231cac0b92cc8f5
MohamedYousry1 <mohamedyosry701@gmail.com>
2026-05-14 16:11:46 +0300

    use ->id (->), instead of ->['id'] (array)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,18 +8,18 @@
         Post Info
     </div>
     <div class="card-body">
-        <h5 class="card-title">{{$post['title']}}</h5>
-        <p class="card-text">{{$post['description']}}</p>
+        <h5 class="card-title">{{$post->title}}</h5>
+        <p class="card-text">{{$post->description}}</p>
     </div>
 </div>
 <div class="card mt-4">
     <div class="card-header">
-        {{ $user['name'] }} Post Info
+        Mohamed Post Info
     </div>
     <div class="card-body">
-        <h5 class="card-title">{{$user['name']}}</h5>
-        <p class="card-text">{{$user['email']}}</p>
-        <p class="card-text">{{$post['created_at']}}</p>
+        <h5 class="card-title">Name: Mohamed</h5>
+        <p class="card-text">Email: mohamedyo44@test.com</p>
+        <p class="card-text">Created At: Thursday 14th of may 2026</p>
     </div>
 </div>
 @endsection
