70e6ee694546e4f19421c981fb0a02fa9129ad33
MohamedYousry1 <mohamedyosry701@gmail.com>
2026-05-14 18:01:01 +0300

    retrieve all users from users table from database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,38 +7,31 @@ use Illuminate\Http\Request;
 use function Laravel\Prompts\info;
 
 use App\Models\Post;
-use App\Models\Customer;
+
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
-        // get all data from database
-            // id,title(VARCHAR),description(TEXT),created_at,updated_at
-
-            $postsFromDB = Post::all(); 
-            return view('posts.index', ['posts' => $postsFromDB]);
-        // $allPosts = [
-        //     ['id' => 1, 'title' => 'PHP', 'posted_by' => 'Mohamed', 'created_at' => '22-10-10 09:00:00'],
-        //     ['id' => 2, 'title' => 'Javascript', 'posted_by' => 'Yassien', 'created_at' => '25-03-18 04:00:00'],
-        //     ['id' => 3, 'title' => 'HTML', 'posted_by' => 'Maged', 'created_at' => '23-11-25 02:00:00'],
-        //     ['id' => 4, 'title' => 'CSS', 'posted_by' => 'Abdo', 'created_at' => '26-03-19 01:15:00'],
-        //     ['id' => 5, 'title' => 'Laravel', 'posted_by' => 'Khaled', 'created_at' => '26-01-01 11:15:00']
-        // ];
-        // return view('posts.index', ['posts' => $allPosts]);
+        // select * from posts;
+        $postsFromDB = Post::all();
+        return view('posts.index', ['posts' => $postsFromDB]);
     }
 
     // Show Action
-    public function show(Post $post) // This way called Route Model Binding
+    public function show(Post $post) // This way called Route Model Binding (Implicit Blinding)
     {
         // select * from posts where id = $postId;
         return view('posts.show', ['post' => $post]);
     }
 
     // Create Action
-    public function create() // this is how to get the post id or URL Parameter value in variable then return this var
-    {
-        return view('posts.create');
+    public function create()
+    {        
+        // select * from users
+        $usersFromDB = User::all();
+        return view('posts.create', ['users' => $usersFromDB]);
     }
 
     // Store Action
@@ -68,18 +61,19 @@ class PostController extends Controller
     // Update Action
     public function update()
     {
-     // 1- get the user data
+        // 1- get the user data
         // $data = request()->all();
         // $title   = request()->title;
         // $description = request()->description;
         // $postCreator = request()->post_creator;
         // dd($title, $description, $postCreator);
-    // 2- Update the user data in database
-    // 3- redirection to posts.index
-        return to_route('posts.show',1);
+        // 2- Update the user data in database
+        // 3- redirection to posts.index
+        return to_route('posts.show', 1);
     }
 
-    public function destroy(){
+    public function destroy()
+    {
         // 1- delete from DataBase
         // 2- Redirect to posts.index
         return to_route('posts.index');
