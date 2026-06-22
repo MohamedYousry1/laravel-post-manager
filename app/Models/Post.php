e5cef2785d557b89be290a7541944c4bbc1ca940
MohamedYousry1 <mohamedyosry701@gmail.com>
2026-06-22 19:17:03 +0300

    feat: fillable to protect from mass assignment vulnerabilities

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+// #[Fillable(['title'])]
 class Post extends Model
 {
-    //
+    // protected from mass assignment vulnerabilities
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id'
+    ];
+ 
+    // If you choose to unguard your model, you should take special care to always hand-craft the arrays passed to Eloquent's fill, create, and update methods:
+    // protected $guarded = []; 
 }
