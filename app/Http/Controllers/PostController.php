1eb67bb76bdcbc297cd5788f43e240a24205ad1e
MohamedYousry1 <mohamedyosry701@gmail.com>
2026-06-25 04:08:43 +0300

    Create post validate, update post validate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use function Laravel\Prompts\info;
+use Illuminate\Http\RedirectResponse;
 
 use App\Models\Post;
 
 use App\Models\User;
+
 
 class PostController extends Controller
 {
@@ -19,36 +19,39 @@ class PostController extends Controller
         return view('posts.index', ['posts' => $postsFromDB]);
     }
 
-    // Show Action
     public function show(Post $post) // This way called Route Model Binding (Implicit Blinding)
     {
         // select * from posts where id = $postId;
         return view('posts.show', ['post' => $post]);
     }
 
-    // Create Action
     public function create()
-    {        
+    {
         // select * from users
         $usersFromDB = User::all();
         return view('posts.create', ['users' => $usersFromDB]);
     }
 
-    // Store Action
-    public function store()
+    public function store(Request $request): RedirectResponse
     {
+
+        // validate data
+        $validated = $request->validate([
+            'title' => 'required|max:50',
+            'description' => 'required|max:255',
+            'post_creator' => 'required|exists:users,id' // exists:table,column
+        ]);
+
         // 1- get the user data
-        // $request = request();  // global helper method
-        $data = request()->all(); // $data = $_POST (in php native) && all() ->> Method of Eloquent Model
         $title   = request()->title;
         $description = request()->description;
         $postCreator = request()->post_creator;
 
 
         $post = Post::create([
-            'title' => $title,
-            'description' => $description,
-            'user_id' => $postCreator
+            'title' => $validated['title'],
+            'description' => $validated['description'],
+            'user_id' => $validated['post_creator']
         ]);
         // 2- store the submitted data in database
 
@@ -56,7 +59,6 @@ class PostController extends Controller
         return to_route('posts.index');
     }
 
-    // Edit Action
     public function edit(Post $post)
     {
         // select * from users
@@ -64,22 +66,37 @@ class PostController extends Controller
         return view('posts.edit', ['users' => $usersFromDB, 'post' => $post]);
     }
 
-    // Update Action
     public function update($postId)
     {
-        // 1- get the data to update
+        // 1- get the data to update -- data from inputs
         $title   = request()->title;
         $description = request()->description;
         $postCreator = request()->post_creator;
-        // 1- select * from posts where id = $postId;
-        $singlePostFromDB = Post::find($postId);
-        // 2- Update the submited user data in database
-        $singlePostFromDB->update([
-            'title'=>$title,
-            'description'=>$description,
-            'user_id' => $postCreator
 
+        // 1- select * from posts where id = $postId;
+        $post = Post::find($postId);
+
+        //Data Validation
+        if (
+            $post->title === $title &&
+            $post->description === $description &&
+            $post->user_id == $postCreator
+        ) {
+            return back()->with('info', 'Nothing changed.');
+        }
+        $validated = request()->validate([
+            'title' => 'required|max:50',
+            'description' => 'required|max:255',
+            'post_creator' => 'required|exists:users,id' // exists:table,column
         ]);
+
+        // 2- Update the Validate Submited post data in database
+        $post->update([
+            'title' => $validated['title'],
+            'description' => $validated['description'],
+            'user_id' => $validated['post_creator']
+        ]);
+
         // 3- redirection to posts.index
         return to_route('posts.show', $postId);
     }
@@ -87,12 +104,8 @@ class PostController extends Controller
     public function destroy($postId)
     {
         // 1- delete from DataBase
-          //select or find post from database
-          //delete the post from database
         $post = Post::find($postId);
         $post->delete();
-
-        // 2- Redirect to posts.index
         return to_route('posts.index', $postId);
     }
 }
