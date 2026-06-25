d984cad0b7f24463e6b3cc6d89e2e1eb347b982f
MohamedYousry1 <mohamedyosry701@gmail.com>
2026-06-25 04:11:49 +0300

    handel errors

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,19 +8,34 @@
     @csrf
     <div class="mt-4">
         <label class="form-label">Title</label>
-        <input type="text" class="form-control" name="title">
+        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}">
+        @error('title')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
     <div class="mt-4">
         <label class="form-label">Description</label>
-        <textarea class="form-control" rows="3" name="description"></textarea>
+        <textarea class="form-control @error('description') is-invalid @enderror" rows="3" name="description">{{ old('description') }}</textarea>
+        @error('description')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
     <div class="mt-4 mb-4">
         <label class="form-label">Post Creator</label>
-        <select class="form-control" name="post_creator">
+        <select class="form-control  @error('post_creator') is-invalid @enderror" name="post_creator">
             @foreach ($users as $user)
             <option value="{{ $user->id }}"> {{$user->name}} </option>
             @endforeach
         </select>
+        @error('post_creator')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
     <button class="btn btn-success">Submit</button>
 </form>
